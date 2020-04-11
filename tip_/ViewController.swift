351c25fbdfd285ab56ed295b4f92a1b352234ce0
Angelo Basa <ambasa@uci.edu>
2020-04-10 17:42:01 -0700

    Initial Commit

---
+++ tip_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_
+//
+//  Created by Angelo Basa on 4/10/20.
+//  Copyright © 2020 Angelo Basa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tip_Tests/tip_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tip_Tests.swift
+//  tip_Tests
+//
+//  Created by Angelo Basa on 4/10/20.
+//  Copyright © 2020 Angelo Basa. All rights reserved.
+//
+
+import XCTest
+@testable import tip_
+
+class tip_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
